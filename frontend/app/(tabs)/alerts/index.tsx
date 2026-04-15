16a9c3c83cf7c3d27265e63bab76e018f427107a
Sai <gementizasgg08@gmail.com>
2026-04-15 23:22:57 +0800

    feat: configure routes for tabs

---
+++ frontend/app/(tabs)/alerts/index.tsx
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+export default function Alerts() {
+  return (
+    <Text>Reports page</Text>
+  )
+}
---
+++ frontend/app/(tabs)/report/index.tsx
@@ -0,0 +1,8 @@
+import { Text } from "react-native";
+
+
+export default function AddReport() {
+  return (
+    <Text className = "text-white">Report</Text>
+  )
+}
